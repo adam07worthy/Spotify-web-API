847b1cbb9b6da728522b8a72f30322c4067e84c5
adam07worthy <adamworthy07@gmail.com>
2025-07-31 23:07:56 +0100

    Add playlist url input inside of GUI window

--- Spotify_API.py
+++ Spotify_API.py
@@ -21,27 +21,39 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     scope="playlist-read-private"
 ))
 
-# Get the songs and artists from a given playlist
-playlist_url = input("Enter playlist URL: ")
-results = sp.playlist_items(playlist_url)
-all_songs = results["items"]
- 
-# Allows for playlists larger than 100 songs
-while results['next']:
-    results = sp.next(results)
-    all_songs.extend(results['items'])
+
 
 # Create a window
 window = tkinter.Tk()
 window.title("Song List")
 window.geometry("500x500")
 
+# Get the songs and artists from a given playlist
+def get_playlist_songs():
+    # Get the playlist URL
+    playlist_url = url_entry.get()
+    # Get the playlist songs and artists
+    results = sp.playlist_items(url_entry.get())
+    all_songs = results["items"]
+    # Allows for playlists larger than 100 songs
+    while results['next']:
+        results = sp.next(results)
+        all_songs.extend(results['items'])
+    # Add songs and artists to window
+    for item in all_songs:
+        display_area.insert(tkinter.END, (item["track"]["artists"][0]["name"] + ": " + item["track"]["name"]) + "\n")
+
+# Get the playlist URL
+url_label = tkinter.Label(window, text="Enter playlist URL")
+url_label.pack()
+url_entry = tkinter.Entry(window, width=50)
+url_entry.pack()
+url_button = tkinter.Button(window, text="enter", command=get_playlist_songs)
+url_button.pack()
+
+
 # Add scrollable text
 display_area = scrolledtext.ScrolledText(window, wrap=tkinter.WORD, width=70, height=25)
 display_area.pack(padx=10, pady=10, fill=tkinter.BOTH, expand=True)
-
-# Add songs and artists to window
-for item in all_songs:
-    display_area.insert(tkinter.END, (item["track"]["artists"][0]["name"] + ": " + item["track"]["name"]) + "\n")
 
 window.mainloop()
